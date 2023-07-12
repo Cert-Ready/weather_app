ff2247b98bcce8e0f1a0bcaa2e6f02c577b8fef1
Cert-Ready <120624638+Cert-Ready@users.noreply.github.com>
2023-07-12 17:55:26 -0500

    Add cleanup function

--- src/components/modules/SetTitle.js
+++ src/components/modules/SetTitle.js
@@ -1,9 +1,15 @@
 import { useEffect } from "react";
 
 const SetTitle = (title) => {
+  let isMounted = false;
+
   useEffect(() => {
     document.title = title;
   }, [title]);
+
+  return () => {
+    isMounted = true;
+  };
 };
 
 export { SetTitle };
